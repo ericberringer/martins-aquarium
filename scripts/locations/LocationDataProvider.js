e8febef34d62e1c30e3e762607082c7353b70c1e
ericberringer <eric.berringer@gmail.com>
2021-01-14 10:24:23 -0600

    imported LocationList which selects which container to place our html, to main.js where we call the function LocationList(). In location.js there is a function Locations that takes a parameter location, which returns the HTML that will be sent to our selected container. Export Locations to LocationList.js. Created a variable locations=useLocation() which houses each object in our array. locationHTMLRepresentation is a new variable using let and is set equal to an empty string. Used a for..of loop to iterate through locations, and set this to a new variable location. Made locationHTMLRepresentation += to Locations(location) so we can grab each object in our array, pair it with the code created in Location.js. Use contentElement.innerHTML += to a string that houses an <article> with  inside.

--- scripts/locations/LocationDataProvider.js
+++ scripts/locations/LocationDataProvider.js
@@ -1,16 +1,16 @@
 const locationCollection = [
     {
-       image: "",
+       image: "../images/carribbean.jpeg",
        place: "Bermuda",
        subtitle: "Bermuda is home to a variety of exotic fish."
     },
     {
-       image: "",
+       image: "../images/fiji.jpg",
        place: "Fiji",
        subtitle: "Fiji is very far away, they have fish."
     },
     {
-       image: "",
+       image: "../images/crete.jpg",
        place: "Crete",
        subtitle: "I'll give you $10 if you can tell me where Crete is. They too have fish."
     },
--- scripts/locations/LocationList.js
+++ scripts/locations/LocationList.js
@@ -1,12 +1,18 @@
 import { useLocation } from './LocationDataProvider.js'
+import { Locations } from './Location.js'
 
 export const LocationList = () => {
-    const contentElement = document.querySelector(.containerLeft__travelLocations)
+    const contentElement = document.querySelector(".containerLeft__travelLocations")
     const locations = useLocation()
 
+    let locationHTMLRepresentation = ""
+    for(const location of locations) 
+
+
+    locationHTMLRepresentation += Locations(location)
     contentElement.innerHTML += `
     <article class="locationList">
-        All the locations go here
+        ${locationHTMLRepresentation}
     </article>
     `
 }
